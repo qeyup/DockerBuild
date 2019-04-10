c6ab238b6ff5f6b891db69bc9fa7ca7606cbe138
Javier Moreno <jgmore@gmail.com>
2019-04-10 22:42:49 +0200

    Added install script

---
+++ DockerInstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+# Tmp dir
+export TMP_DIR="/tmp/Dockerinstall"
+export EXEC="Dockerfile.sh"
+mkdir -p $TMP_DIR
+
+
+# Find all install script
+find $PWD -name "$EXEC" | while read line; do echo "$(echo $line | grep -o / | wc -l)->$line"; done | sort -rn | while read file
+do
+    # Remove num
+    file=$(echo $file | sed "s/^.*->//g")
+
+    # Generate tmp file
+    CHECK=$(md5sum "$file" | cut -c -32)
+    CHECK="$TMP_DIR/$CHECK"
+
+
+    # Check if file is been executed
+    if [ -e "$CHECK" ]
+    then
+        continue
+    else
+        touch "$CHECK"
+    fi
+
+
+    # Execute
+    (
+        SOURCE_DIR=$(dirname "$file")
+        cd "$SOURCE_DIR"
+        chmod +x $EXEC
+        . $EXEC
+    )
+
+done
+
+
+# Remove temporal files
+rm -rf $TMP_DIR
