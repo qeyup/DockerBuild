54546915e1eed8895cf129d4d1cfba9ed464ddef
Javier Moreno <jgmore@gmail.com>
2019-07-14 21:45:20 +0200

    Fix build mechanims. Updated build rules

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -4,25 +4,30 @@ set -e
 # Tmp dir
 export TMP_DIR="/tmp/Dockerinstall"
 export EXEC="Dockerfile.sh"
+export SORT_STRING="~~~~~~~~"
 mkdir -p $TMP_DIR
 
 
 # Find all install script
-find $PWD -name "$EXEC" | while read line; do echo $line; done | sort | while read file
+find $PWD -name "$EXEC" | sed "s/$EXEC/$SORT_STRING/g" | sort | sed "s/$SORT_STRING/$EXEC/g" | while read file
 do
-    # Remove num
-    file=$(echo $file | sed "s/^.*->//g")
-
     # Generate tmp file
     CHECK=$(md5sum "$file" | cut -c -32)
     CHECK="$TMP_DIR/$CHECK"
 
 
     # Check if file is been executed
+    SEP="============================================================================"
     if [ -e "$CHECK" ]
     then
+        echo "$SEP"
+        echo "[SKIPED] $file"
+        echo "$SEP"
         continue
     else
+        echo "$SEP"
+        echo "[BUILDING] $file"
+        echo "$SEP"
         touch "$CHECK"
     fi
 
@@ -32,8 +37,12 @@ do
         SOURCE_DIR=$(dirname "$file")
         cd "$SOURCE_DIR"
         chmod +x $EXEC
-        /bin/bash -exi -c "source $EXEC"
+        /bin/bash -exi -c "source $EXEC" || ( echo "$SEP" && echo "[ERROR] $file" && echo "$SEP" &&  exit -1; )
     )
+
+    echo "$SEP"
+    echo "[BUILT] $file"
+    echo "$SEP"
 
 done
 
