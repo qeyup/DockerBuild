98af76469a0a494e2bfd2af80d5c70061a31a3f7
Javier Moreno <jgmore@gmail.com>
2020-01-25 01:36:52 +0100

    Update

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -2,57 +2,107 @@
 set -e
 
 
+#> Log
+function log {
+    echo "$@"
+}
 
-#~~~ CONFIGURABLE PARAMETERS ~~~~~~~~~~~~~~
-#> base image
-DOCKER_BASE_IMAGE="ubuntu:18.04"
-
-#> base image
-DOCKER_IMAGE_NAME="docker_image_name"
-
-# Added args when building de image
-DOCKER_BUILD_ARGS=""
-
-# Added lines at the top of the generated Dockerfile
-DOCKER_HEADER_FILE=""
-
-# Added lines at the buttom of the generated Dockerfile
-DOCKER_TAIL_FILE=""
-
-# Skip similar bash files (true/false)
-SKIP=true
-
-# File to find and execute
-EXEC="Dockerfile.sh"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-##########################################
-#> Tmp dir
+#> Variables
 TMP_DIR=".added/"
 TMP_DOCKER_FOLDER="/tmp/docker_build"
 SORT_STRING="zzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+EXEC="Dockerfile.sh"
+CREATED_DOCKER_FILE=".Dockerfile"
+GENERATE_CONTENT_LABEL="# [GENERATED CONTENT SPACE (DO NOT REMOVE THIS LINE)]"
+
+#> Usage
+if [ "$1" = "" ]
+then
+    log ""
+    log " Usage: $0 [Dockerfile_path] [docker_build_args]"
+    log ""
+    exit 0
+fi
+
+
+#> Dockerfile path 
+if [ -f "$1" ]
+then
+    DOCKERFILE_PATH="$1"
+elif [ -d "$1" ]
+then
+    DOCKERFILE_PATH="$1/Dockerfile"
+else
+    log "al least Dockerfile path must be specify"
+    exit -1
+fi
+
+
+# Create Dockerfile template
+if [ ! -f "$DOCKERFILE_PATH" ]
+then
+    DOCKERFILE_CONTEND=""
+    DOCKERFILE_CONTEND+="FROM ubuntu:18.04\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="########################################\n"
+    DOCKERFILE_CONTEND+="# Docker image name\n"
+    DOCKERFILE_CONTEND+="#DB DOCKER_IMAGE_NAME=docker_image_name\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="# Docker build defined args\n"
+    DOCKERFILE_CONTEND+="#DB DOCKER_BUILD_ARGS=\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="# Skip similar found modules\n"
+    DOCKERFILE_CONTEND+="#DB SKIP=true\n"
+    DOCKERFILE_CONTEND+="########################################\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="$GENERATE_CONTENT_LABEL"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="WORKDIR /root/\n"
+    DOCKERFILE_CONTEND+="\n"
+
+    echo -e "$DOCKERFILE_CONTEND" > $DOCKERFILE_PATH
+    log "Created template Dokerfile. Configure it and execute again '$0$@' to build de docker image."
+    exit 0
+fi
+
+
+# Read variables
+VARS_ARRAY=$(cat $DOCKERFILE_PATH | grep "#DB " | sed "s/^#DB //g" | while read var; do echo $var; done; )
+
+# export vars
+for VAR in $VARS_ARRAY
+do
+    export $VAR
+done
+
+
+# Check 
+if [ "$DOCKER_IMAGE_NAME" = "" ]
+then
+    log "DOCKER_IMAGE_NAME valiable is missing"
+    exit -1
+fi
+
+
+#> Tmp dir
 rm -rf ${TMP_DIR}
 mkdir -p ${TMP_DIR}
 
 
 #> Create main docker file
-rm -f Dockerfile
-touch Dockerfile
-echo "FROM ${DOCKER_BASE_IMAGE}" >> Dockerfile
-echo "" >> Dockerfile
-echo "" >> Dockerfile
+rm -f ${CREATED_DOCKER_FILE}
+touch ${CREATED_DOCKER_FILE}
 
-#> Add header file
-if [ -f "${DOCKER_HEADER_FILE}" ]
-then
-    cat ${DOCKER_HEADER_FILE} >> Dockerfile
-    echo "" >> Dockerfile
-    echo "" >> Dockerfile
-fi
+
+#> Find files and sort them
+FOUND_FILES=$(find $PWD -name "$EXEC" | sed "s/$EXEC/$SORT_STRING/g" | sort | sed "s/$SORT_STRING/$EXEC/g" | while read file; do echo ${file}; done;)
+
 
 #> Find build scripts
-find $PWD -name "$EXEC" | sed "s/$EXEC/$SORT_STRING/g" | sort | sed "s/$SORT_STRING/$EXEC/g" | while read file
+for file in $FOUND_FILES
 do
     #> Check
     if [ "$SKIP" = true ] ; then
@@ -70,16 +120,16 @@ do
     fi
 
 
-    #> Exec install
+    #> Exec install steps
     SOURCE_DIR=$(dirname "$file")
     SOURCE_DIR=$(realpath --relative-to=$PWD $SOURCE_DIR)
-    echo "# Building '${SOURCE_DIR}/${EXEC}'" >> Dockerfile
-    echo "RUN mkdir -p ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}" >> Dockerfile
-    echo "COPY ${SOURCE_DIR}/. ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/" >> Dockerfile
-    echo "RUN chmod u+x ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/${EXEC}" >> Dockerfile
-    echo "RUN cd ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/ && /bin/bash -iex -c \"source ${EXEC}\"" >> Dockerfile
-    echo "" >> Dockerfile
-    echo "" >> Dockerfile
+    DOCKERFILE_CONTEND+="# Building '${SOURCE_DIR}/${EXEC}'\n"
+    DOCKERFILE_CONTEND+="RUN mkdir -p ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}\n"
+    DOCKERFILE_CONTEND+="COPY ${SOURCE_DIR}/. ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/\n"
+    DOCKERFILE_CONTEND+="RUN chmod u+x ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/${EXEC}\n"
+    DOCKERFILE_CONTEND+="RUN cd ${TMP_DOCKER_FOLDER}/${SOURCE_DIR}/ && /bin/bash -iex -c \"source ${EXEC}\"\n"
+    DOCKERFILE_CONTEND+="\n"
+    DOCKERFILE_CONTEND+="\n"
 
 
     #> Register
@@ -90,26 +140,36 @@ do
 done
 
 
-#> Clean
-echo "RUN rm -rf ${TMP_DOCKER_FOLDER}" >> Dockerfile
+#> Clean step
+DOCKERFILE_CONTEND+="RUN rm -rf ${TMP_DOCKER_FOLDER}"
 
 
-#> Add tail
-if [ -f "${DOCKER_HEADER_FILE}" ]
-then
-    cat ${DOCKER_HEADER_FILE} >> Dockerfile
-    echo "" >> Dockerfile
-    echo "" >> Dockerfile
-fi
+#> Put generated code in file
+cat $DOCKERFILE_PATH | while read LINE
+do
+    if [ "$LINE" = "${GENERATE_CONTENT_LABEL}" ]
+    then
+        echo -e "${DOCKERFILE_CONTEND}" >> ${CREATED_DOCKER_FILE}
+        DOCKERFILE_CONTEND=""
+    else
+        echo -e "${LINE}" >> ${CREATED_DOCKER_FILE}
+    fi
+done
 
 
 # Execute docker build
 (
-    set -x
-    sudo docker build -t $DOCKER_IMAGE_NAME $DOCKER_BUILD_ARGS .
+    if [ "$EUID" -ne 0 ]
+    then
+        set -x
+        sudo docker build -t $DOCKER_IMAGE_NAME -f ${CREATED_DOCKER_FILE} $DOCKER_BUILD_ARGS .
+    else
+        set -x
+        docker build -t $DOCKER_IMAGE_NAME -f ${CREATED_DOCKER_FILE} $DOCKER_BUILD_ARGS .
+    fi
 )
 
 
 # Remove temporal files
 rm -r ${TMP_DIR}
-#rm Dockerfile
+rm ${CREATED_DOCKER_FILE}
