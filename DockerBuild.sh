aba7d482a947cfad9c1659a578dc8fef82c9df0b
Javier Moreno <jgmore@gmail.com>
2020-02-09 12:58:36 +0100

    Update. Added build source and image source features

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -10,14 +10,17 @@ function log {
 #> Variables
 TMP_DOCKER_FOLDER="/tmp/docker_build"
 SORT_STRING="zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
-EXEC="Dockerfile.sh"
-EXEC_DEBUG="Dockerfile.debug.sh"
 EXEC_SORT_KEY="Dockerfile."
+EXEC="${EXEC_SORT_KEY}sh"
+EXEC_DEBUG="${EXEC_SORT_KEY}debug.sh"
+BUILD_EXPORT_SOURCE="${EXEC_SORT_KEY}buildExport.source"
+IMAGE_EXPORT_SOURCE="${EXEC_SORT_KEY}imageExport.source"
 CREATED_DOCKER_FILE=".Dockerfile"
 GENERATE_CONTENT_LABEL="# [DO NOT REMOVE THIS LINE. THIS LINE WILL BE REMPLACED WITH GENERATED CODE]"
 DEFAULT_DOCKERFILE_PATH="$PWD"
 DOCKER_DEBUG_FOLDER="/tmp/debug_folder"
 DOCKER_WORKSPACE="/root/workspace"
+BUILD_SOURCE_FILE=${TMP_DOCKER_FOLDER}/dockerBuild.source
 
 
 # Process args
@@ -26,9 +29,14 @@ do
     case $opt in
         # Help
         h)
-            log "The script will search for 'Dockerfile.sh' scripts and append them in the main Dockerfile before build it."
-            log "* It will take into acount the found 'Dockerfile.sh' files hierarchy order."
-            log "* To debug an 'Dockerfile.sh' script just set the script name as 'Dockerfile.debug.sh'."
+            log "DockerBuild script is meant to help the docker build process. Is just a wrapper of the docker build process."
+            log "It will look for '${EXEC}', '${BUILD_EXPORT_SOURCE}' and '${IMAGE_EXPORT_SOURCE}' files and add them to the docker image build steps sorting them by name and nesting level position."
+            log ""
+            log ""
+            log "File description"
+            log "  ${EXEC}: shell script that will be executed in a docker build step. In order to debug it, change the file name to '${EXEC_DEBUG}'."
+            log "  ${BUILD_EXPORT_SOURCE}: Source file that will be included only in the build process."
+            log "  ${IMAGE_EXPORT_SOURCE}: Source file that will be included for the docker container execution and build process."
             log ""
             log ""
             log " DockerBuild.sh comand args help"
@@ -153,40 +161,99 @@ rm -f ${CREATED_DOCKER_FILE}
 touch ${CREATED_DOCKER_FILE}
 
 
+#> Make docker build folder and create an empy build source file
+DOCKERFILE_CONTEND+="# Make docker build folder and create an empy build source file\n"
+DOCKERFILE_CONTEND+="RUN mkdir -p \"${TMP_DOCKER_FOLDER}/\"\n"
+DOCKERFILE_CONTEND+="RUN touch \"${BUILD_SOURCE_FILE}\"\n"
+DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo\" >> /etc/bash.bashrc\n"
+DOCKERFILE_CONTEND+="\n"
+DOCKERFILE_CONTEND+="\n"
+
+
 #> Find files and sort them
 IFS=";"
-FOUND_FILES=$(find $DOCKERFILE_SCRIPTS_START_SEARCH -name "$EXEC" -or -name "$EXEC_DEBUG" | sed "s/$EXEC_SORT_KEY/$SORT_STRING/g" | sort | sed "s/$SORT_STRING/$EXEC_SORT_KEY/g" | while read file; do echo -ne "${file};"; done;)
+FOUND_FILES=$(find $DOCKERFILE_SCRIPTS_START_SEARCH -name "$EXEC_SORT_KEY*" | sed "s/$EXEC_SORT_KEY/$SORT_STRING/g" | sort | sed "s/$SORT_STRING/$EXEC_SORT_KEY/g" | while read file; do echo -ne "${file};"; done;)
 
 
 #> Find build scripts
 counter=0
 for file in $FOUND_FILES
 do
-    #> Check debug
-    if [ "$(basename ${file})" == "$EXEC_DEBUG" ]
+    
+    #> Get work dir
+    SOURCE_DIR=$(dirname "$file")
+    SOURCE_DIR=$(realpath --relative-to=$PWD "$SOURCE_DIR")
+    if  [ ! "$SOURCE_DIR" = "$LAST_SOURCE_DIR" ]
     then
-        log "[DEBUG]  ${file}"
-        DEBUG_FOLDER=$(realpath $(dirname "$file"))
-        DOCKERFILE_CONTEND+="ENTRYPOINT cp -r ${DOCKER_DEBUG_FOLDER}/* ${DOCKER_WORKSPACE}/ && ln -sf ${DOCKER_DEBUG_FOLDER}/${EXEC_DEBUG} ${DOCKER_WORKSPACE}/${EXEC_DEBUG}&& bash \n"
-        break
+        LAST_SOURCE_DIR=$SOURCE_DIR
+        counter=$((counter+1))
+        DOCKERFILE_CONTEND+="# Copy source files '${SOURCE_DIR}'\n"
+        DOCKERFILE_CONTEND+="RUN mkdir -p \"${TMP_DOCKER_FOLDER}/${counter}\"\n"
+        DOCKERFILE_CONTEND+="COPY [\"${SOURCE_DIR}/.\", \"${TMP_DOCKER_FOLDER}/${counter}/\"]\n"
+        DOCKERFILE_CONTEND+="\n"
+        DOCKERFILE_CONTEND+="\n"
     fi
 
 
-    #> Exec install steps
-    counter=$((counter+1))
-    SOURCE_DIR=$(dirname "$file")
-    SOURCE_DIR=$(realpath --relative-to=$PWD "$SOURCE_DIR")
-    DOCKERFILE_CONTEND+="# Building '${SOURCE_DIR}/${EXEC}'\n"
-    DOCKERFILE_CONTEND+="RUN mkdir -p \"${TMP_DOCKER_FOLDER}/${counter}\"\n"
-    DOCKERFILE_CONTEND+="COPY [\"${SOURCE_DIR}/.\", \"${TMP_DOCKER_FOLDER}/${counter}/\"]\n"
-    DOCKERFILE_CONTEND+="RUN chmod u+x \"${TMP_DOCKER_FOLDER}/${counter}/${EXEC}\"\n"
-    DOCKERFILE_CONTEND+="RUN cd \"${TMP_DOCKER_FOLDER}/${counter}/\" && /bin/bash -iex -c \"source ${EXEC}\"\n"
-    DOCKERFILE_CONTEND+="\n"
-    DOCKERFILE_CONTEND+="\n"
+    #> Check debug
+    if [ "$(basename ${file})" = "$EXEC_DEBUG" ]
+    then
+        log "[DEBUG]  ${file}"
+        DEBUG_FOLDER=$(realpath $(dirname "$file"))
+        DOCKERFILE_CONTEND+="RUN cat ${BUILD_SOURCE_FILE} >> /etc/bash.bashrc\n"
+        DOCKERFILE_CONTEND+="ENTRYPOINT cp -r ${DOCKER_DEBUG_FOLDER}/* ${DOCKER_WORKSPACE}/ && ln -sf ${DOCKER_DEBUG_FOLDER}/${EXEC_DEBUG} ${DOCKER_WORKSPACE}/${EXEC_DEBUG} && bash \n"
+
+        log "[DOCKERFILE DEBUG STEP]  ${file}"
+
+        break
 
 
-    #> Register
-    log "[ADDED]  ${file}"
+    elif [ "$(basename ${file})" = "$EXEC" ]
+    then
+        #> Exec install steps
+        DOCKERFILE_CONTEND+="# Building '${SOURCE_DIR}/${EXEC}'\n"
+        DOCKERFILE_CONTEND+="RUN chmod u+x \"${TMP_DOCKER_FOLDER}/${counter}/${EXEC}\"\n"
+        DOCKERFILE_CONTEND+="RUN echo \"Building '${SOURCE_DIR}/${EXEC}'...\"\n"
+        DOCKERFILE_CONTEND+="RUN cd \"${TMP_DOCKER_FOLDER}/${counter}/\" && /bin/bash -ex -c \"source ${BUILD_SOURCE_FILE} && ./${EXEC}\"\n"
+        DOCKERFILE_CONTEND+="\n"
+        DOCKERFILE_CONTEND+="\n"
+
+        log "[DOCKERFILE STEP ADDED]  ${file}"
+
+
+    elif [ "$(basename ${file})" = "$BUILD_EXPORT_SOURCE" ]
+    then
+        #> Add source to source file
+        DOCKERFILE_CONTEND+="# Append build source file '${TMP_DOCKER_FOLDER}/${counter}/${BUILD_EXPORT_SOURCE}'\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo '# Source from ${SOURCE_DIR}/${BUILD_EXPORT_SOURCE}' \" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"cat ${TMP_DOCKER_FOLDER}/${counter}/${BUILD_EXPORT_SOURCE}\" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo \" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo \" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="\n"
+        DOCKERFILE_CONTEND+="\n"
+
+        log "[BUILD SOURCE ADDED   ]  ${file}"
+
+
+    elif [ "$(basename ${file})" == "$IMAGE_EXPORT_SOURCE" ]
+    then
+        #> Add source to source file and to bash rc file
+        DOCKERFILE_CONTEND+="# Append image source file '${TMP_DOCKER_FOLDER}/${counter}/${IMAGE_EXPORT_SOURCE}'\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo '# Source from ${SOURCE_DIR}/${IMAGE_EXPORT_SOURCE}' \" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"cat ${TMP_DOCKER_FOLDER}/${counter}/${IMAGE_EXPORT_SOURCE}\" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo\" >> ${BUILD_SOURCE_FILE}\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo\" >> ${BUILD_SOURCE_FILE}\n"
+
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo '# Source from ${SOURCE_DIR}/${IMAGE_EXPORT_SOURCE}' \" >> /etc/bash.bashrc\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"cat ${TMP_DOCKER_FOLDER}/${counter}/${IMAGE_EXPORT_SOURCE}\" >> /etc/bash.bashrc\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo\" >> /etc/bash.bashrc\n"
+        DOCKERFILE_CONTEND+="RUN /bin/bash -c \"echo\" >> /etc/bash.bashrc\n"
+        DOCKERFILE_CONTEND+="\n"
+        DOCKERFILE_CONTEND+="\n"
+
+        log "[IMAGE SOURCE ADDED   ]  ${file}"
+    fi
+
 done
 
 
